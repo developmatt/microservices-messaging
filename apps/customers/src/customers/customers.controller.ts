b4c87604b9ef2eeb98c2ed18a2a0455518ea08cb
Matheus Godoi <hi.developmatt@gmail.com>
2024-12-21 11:01:45 -0300

    feature - implements guards

--- apps/customers/src/customers/customers.controller.ts
+++ apps/customers/src/customers/customers.controller.ts
@@ -7,10 +7,12 @@ import {
   Param,
   Delete,
   HttpCode,
+  UseGuards,
 } from '@nestjs/common';
 import { CustomersService } from './customers.service';
 import { CreateCustomerDto } from './dto/create-customer.dto';
 import { UpdateCustomerDto } from './dto/update-customer.dto';
+import { AuthGuard } from 'src/auth/auth.guard';
 
 @Controller('customers')
 export class CustomersController {
@@ -21,16 +23,19 @@ export class CustomersController {
     return this.customersService.create(createCustomerDto);
   }
 
+  @UseGuards(AuthGuard)
   @Get()
   findAll() {
     return this.customersService.findAll();
   }
 
+  @UseGuards(AuthGuard)
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.customersService.findOne(id);
   }
 
+  @UseGuards(AuthGuard)
   @Patch(':id')
   update(
     @Param('id') id: string,
@@ -39,6 +44,7 @@ export class CustomersController {
     return this.customersService.update(id, updateCustomerDto);
   }
 
+  @UseGuards(AuthGuard)
   @HttpCode(204)
   @Delete(':id')
   remove(@Param('id') id: string) {
