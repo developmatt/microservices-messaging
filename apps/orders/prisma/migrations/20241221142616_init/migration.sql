37a566e36069ff70acc49e068a08b2de767ac70d
Matheus Godoi <hi.developmatt@gmail.com>
2024-12-21 11:26:44 -0300

    feature - bootstrap project

---
+++ apps/orders/prisma/migrations/20241221142616_init/migration.sql
@@ -0,0 +1,15 @@
+-- CreateTable
+CREATE TABLE "Order" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    "total" REAL NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "Customer" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    "externalId" TEXT NOT NULL
+);
