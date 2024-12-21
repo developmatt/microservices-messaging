f810f235b596d06c2360d843e5fa4eaafa029d0e
Matheus Godoi <hi.developmatt@gmail.com>
2024-12-21 12:01:18 -0300

    feature - add orders crud

--- apps/orders/prisma/migrations/20241221144205_init/migration.sql
+++ apps/orders/prisma/migrations/20241221144205_init/migration.sql
@@ -3,7 +3,9 @@ CREATE TABLE "Order" (
     "id" TEXT NOT NULL PRIMARY KEY,
     "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" DATETIME NOT NULL,
-    "total" REAL NOT NULL
+    "total" REAL NOT NULL,
+    "customerId" TEXT NOT NULL,
+    CONSTRAINT "Order_customerId_fkey" FOREIGN KEY ("customerId") REFERENCES "Customer" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
 );
 
 -- CreateTable
--- apps/orders/src/app.module.ts
+++ apps/orders/src/app.module.ts
@@ -2,12 +2,18 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { OrdersModule } from './orders/orders.module';
 import configuration from './config/configuration';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({
-    load: [configuration]
-  })],
+  imports: [
+    ConfigModule.forRoot({
+      load: [configuration],
+    }),
+    OrdersModule,
+    PrismaModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
